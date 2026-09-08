29460a364c201717136ed298745899f1e75304d1
mailong2401 <gtmailong@gmail.com>
2026-09-08 20:22:51 +0700

    udpate

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import SkillsSection from '@/components/sections/SkillsSection';
 import ExperienceSection from '@/components/sections/ExperienceSection';
 import ProjectsSection from '@/components/sections/ProjectsSection';
 import AchievementsSection from '@/components/sections/AchievementsSection';
-import BlogsSection from '@/components/sections/BlogsSection';
 import ContactSection from '@/components/sections/ContactSection';
 import Footer from '@/components/layout/Footer';
 import PlanetEarth from '@/components/ui/PlanetEarth';
